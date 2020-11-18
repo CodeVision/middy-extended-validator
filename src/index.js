269760c4bf65de787173315eebe612b514ea8dce
CodeVision <codevision@gmail.com>
2020-11-18 10:31:21 +0100

    bug: fix message for root properties

--- src/index.js
+++ src/index.js
@@ -15,6 +15,21 @@ const bodyMount = schema => ({
   required: ['body'],
 });
 
+const transformPath = (datapath, { mountSchemaAtBody }) => {
+  // transform dataPath from '/body/something' to '.body.something' or
+  // just '.something' (if body was inserted previously)
+  let sliceIndex = 0;
+  if (mountSchemaAtBody) {
+    const slashIndex = datapath.indexOf('/', 2);
+    if (slashIndex >= 0) {
+      sliceIndex = slashIndex;
+    } else {
+      sliceIndex = datapath.length;
+    }
+  }
+  return datapath.slice(sliceIndex).replace(/\//g, '.');
+};
+
 const validator = options => {
   const { mountSchemaAtBody, detailedErrors } = options;
 
@@ -29,10 +44,7 @@ const validator = options => {
         const detailedMessage = {
           message: handler.error.message,
           details: handler.error.details.map(detail => {
-            // transform dataPath from '/body/something' to '.body.something' or
-            // just '.something' (if body was inserted previously)
-            const sliceIndex = mountSchemaAtBody ? detail.dataPath.indexOf('/', 2) : 1;
-            const transformedPath = detail.dataPath.slice(sliceIndex).replace(/\//g, '.');
+            const transformedPath = transformPath(detail.dataPath, { mountSchemaAtBody });
             return `${transformedPath} ${detail.message}`;
           }),
         };
